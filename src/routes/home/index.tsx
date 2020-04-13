dc2468c69e97f5cca4b82990663d947ca047cc9f
Matthew Harwood <harwood@uber.com>
2020-04-13 10:30:07 -0700

    init commit

---
+++ src/routes/home/index.tsx
@@ -0,0 +1,16 @@
+import { FunctionalComponent, h } from "preact";
+import * as style from "./style.css";
+
+const Home: FunctionalComponent = () => {
+    return (
+        <div class={style.home}>
+            <h1>Home</h1>
+            <p>This is the Home component.</p>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Button
+            </button>
+        </div>
+    );
+};
+
+export default Home;
